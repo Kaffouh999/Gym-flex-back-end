9e8db0a79c4305dac81ad01a69b832f4e43d907d
othman <othmanekaffouh2001@gmail.com>
2023-03-30 15:43:49 +0000

    adding product crud backend

---
+++ src/main/java/com/example/GymInTheBack/services/mappers/ProductMapper.java
@@ -0,0 +1,21 @@
+package com.example.GymInTheBack.services.mappers;
+
+
+import com.example.GymInTheBack.dtos.product.ProductDTO;
+import com.example.GymInTheBack.dtos.subCategory.SubCategoryDTO;
+import com.example.GymInTheBack.entities.Product;
+import com.example.GymInTheBack.entities.SubCategory;
+import com.example.GymInTheBack.utils.EntityMapper;
+import org.mapstruct.*;
+
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper extends EntityMapper<ProductDTO, Product> {
+    @Mapping(target = "subCategory", source = "subCategory", qualifiedByName = "subCategoryId")
+    ProductDTO toDto(Product s);
+
+    @Named("subCategoryId")
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "id", source = "id")
+    SubCategoryDTO toDtoSubCategoryId(SubCategory subCategory);
+}
