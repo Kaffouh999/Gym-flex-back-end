40af7209b2eb712e45daa2ba9a69806b8ee4dbfc
othman <othmanekaffouh2001@gmail.com>
2023-03-31 12:49:30 +0000

    adding cart crud backend

---
+++ src/main/java/com/example/GymInTheBack/services/cart/CartService.java
@@ -0,0 +1,66 @@
+package com.example.GymInTheBack.services.cart;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.GymInTheBack.dtos.cart.CartDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
+public interface CartService {
+    /**
+     * Save a cart.
+     *
+     * @param cartDTO the entity to save.
+     * @return the persisted entity.
+     */
+    CartDTO save(CartDTO cartDTO);
+
+    /**
+     * Updates a cart.
+     *
+     * @param cartDTO the entity to update.
+     * @return the persisted entity.
+     */
+    CartDTO update(CartDTO cartDTO);
+
+    /**
+     * Partially updates a cart.
+     *
+     * @param cartDTO the entity to update partially.
+     * @return the persisted entity.
+     */
+    Optional<CartDTO> partialUpdate(CartDTO cartDTO);
+
+    /**
+     * Get all the carts.
+     *
+     * @return the list of entities.
+     */
+    List<CartDTO> findAll();
+
+    /**
+     * Get all the carts with eager load of many-to-many relationships.
+     *
+     * @param pageable the pagination information.
+     * @return the list of entities.
+     */
+    Page<CartDTO> findAllWithEagerRelationships(Pageable pageable);
+
+    /**
+     * Get the "id" cart.
+     *
+     * @param id the id of the entity.
+     * @return the entity.
+     */
+    Optional<CartDTO> findOne(Long id);
+
+    /**
+     * Delete the "id" cart.
+     *
+     * @param id the id of the entity.
+     */
+    void delete(Long id);
+}
