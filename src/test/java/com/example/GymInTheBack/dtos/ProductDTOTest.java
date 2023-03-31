871b749f3e2e942df134a8a6ea29d083d8c44c02
othman <othmanekaffouh2001@gmail.com>
2023-03-31 14:04:22 +0000

    adding dtoTests

---
+++ src/test/java/com/example/GymInTheBack/dtos/ProductDTOTest.java
@@ -0,0 +1,26 @@
+package com.example.GymInTheBack.dtos;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+import com.example.GymInTheBack.dtos.product.ProductDTO;
+import com.example.GymInTheBack.web.TestUtil;
+import org.junit.jupiter.api.Test;
+
+class ProductDTOTest {
+
+    @Test
+    void dtoEqualsVerifier() throws Exception {
+        TestUtil.equalsVerifier(ProductDTO.class);
+        ProductDTO productDTO1 = new ProductDTO();
+        productDTO1.setId(1L);
+        ProductDTO productDTO2 = new ProductDTO();
+        assertThat(productDTO1).isNotEqualTo(productDTO2);
+        productDTO2.setId(productDTO1.getId());
+        assertThat(productDTO1).isEqualTo(productDTO2);
+        productDTO2.setId(2L);
+        assertThat(productDTO1).isNotEqualTo(productDTO2);
+        productDTO1.setId(null);
+        assertThat(productDTO1).isNotEqualTo(productDTO2);
+    }
+}
